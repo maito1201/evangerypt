3d25fd8dd7fa1130d22a3098c92e3f7a7dba7e03
maito1201 <maito1201@users.noreply.github.com>
2022-10-28 14:43:32 +0000

    deploy: e94f7844fcf187151586743f9c06bb14698445da

---
+++ _next/static/EYnW97YtRnY2rCsYflump/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/25-f2e42b838e78c640.js","static/chunks/pages/index-ded307ce0a401c1d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
