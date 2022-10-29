605f8e4112d83fbccde8e94bfb6f511c20faa256
maito1201 <49754654+maito1201@users.noreply.github.com>
2022-10-29 12:02:22 +0900

    remove statuc generate config (#4)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: '/evangerypt',
-  basePath: '/evangerypt',
-  trailingSlash: true,
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' }
-    }
-  }
+  trailingSlash: true
 }
 
 module.exports = nextConfig
